c831138597c898b92f77aefe2474ede8c8b29be8
kisna <kisna@Kisna-Suji>
2016-12-03 23:43:12 -0500

    Populating the specialities.

--- FindYourDoctorWeb/js/selectYourDoctor.js
+++ FindYourDoctorWeb/js/selectYourDoctor.js
@@ -1,3 +1,16 @@
-/**
- * 
- */
+function populateSpecialities(specialitysSelect) {
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			var selectionElets = JSON.parse(this.responseText).specialities;
+			for (var index = 0; index < selectionElets.length; index++) {
+				var value = selectionElets[index]["specialityId"];
+				var text = selectionElets[index]["specialityName"];
+				specialitysSelect.options[specialitysSelect.options.length] = new Option(text, value);
+			}
+
+		}
+	};
+	xhttp.open("GET", "FindSpecialites.php", true);
+	xhttp.send();
+}
